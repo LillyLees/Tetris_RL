a77959860cf747929d73bd55d64b7904bee9030b
LillyLees <67157476+LillyLees@users.noreply.github.com>
2021-10-07 10:59:00 -0400

    Create main.py
    main.py from vs code. git commit not working. test code with home made enviro in mind

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy import array
+from Tetris import Tetris
+Game = Tetris()
+
+'''for Tet in Game.Tet_Names:
+    print(Tet)
+    for coord in Game.Tetraminos[Tet]:
+        Game.board[coord] = 1
+    print(Game.board)
+    Game.board = array([Game.board_width * [0]] * Game.board_height)''' # Test to see if Tet shapes are right, sucsess
