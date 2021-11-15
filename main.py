7c5f03eab3019cb1ead9c5e713ed6288e6f84072
Lilly swagpants Lees <climbgeekv7@gmail.com>
2021-11-15 12:19:14 +0000

    added conition for checking if a tet has dropped and reset method

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ Game = Tetris()
 new_game = Tetris()
 new_game.make_grid()
 while new_game.playing == True:
-    
     for event in pygame.event.get():  
         if event.type == pygame.QUIT: 
-            print("aaaaaa") 
-            new_game.playing = False  
+            new_game.playing = False 
     new_game.Draw_board()
     new_game.spawn_tet()
+    if new_game.dropped == True:
+        new_game.reset_tet()
+
     pygame.display.flip()
+    #get state, reward, done, action
