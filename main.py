c268dd3ee47e04a96c8b9a23d01b014351ff0b71
LillyLees <67157476+LillyLees@users.noreply.github.com>
2021-11-15 10:00:51 -0500

    detect if tet dropped
    added condition to check if its time to spawn a new tet.

--- main.py
+++ main.py
@@ -10,11 +10,22 @@ Game = Tetris()
 new_game = Tetris()
 new_game.make_grid()
 while new_game.playing == True:
-    
     for event in pygame.event.get():  
         if event.type == pygame.QUIT: 
-            print("aaaaaa") 
-            new_game.playing = False  
+            new_game.playing = False 
+    move = [] #array of two moves 
+    #possible moves: 
+    #H hard drop
+    #L left one
+    #R right one
+    #RL rotate left
+    #RR rotate right 
+    if move == "H":
+        new_game.tet_hard_drop()
     new_game.Draw_board()
     new_game.spawn_tet()
+    if new_game.dropped == True:
+        new_game.reset_tet()
+
     pygame.display.flip()
+    #get state, reward, done, action
