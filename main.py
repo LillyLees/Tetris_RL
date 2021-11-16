61288d7f4fab9201e192eb98abe425d388c12c09
Lilly swagpants Lees <climbgeekv7@gmail.com>
2021-11-16 13:53:26 +0000

    fixed bug in main now generates and spaws tet before game loop

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from Tetris import Tetris
 Game = Tetris()
 new_game = Tetris()
 new_game.make_bit_map()
-
+new_game.generate_new_tet()
+new_game.spawn_tet()
 
 while new_game.playing == True:
     for event in pygame.event.get():  
@@ -22,7 +23,7 @@ while new_game.playing == True:
         new_game.get_move(move) #loop through each move and make the required changed to the bitmap
     new_game.update_tet_position()
 
-    if new_game.dropped == True:
+    '''if new_game.dropped == True:
         new_game.place_tet()
         new_game.clear_line()  #check if there are any lines cleared, if there are this method also adds to the score and calcualtes a new level
         new_game.reset_tet()
@@ -30,6 +31,6 @@ while new_game.playing == True:
     
     new_game.redraw_board()
      #redraw board with new moves 
-    new_game.check_game_end()
+    new_game.check_game_end() '''
     pygame.display.flip()
     #get state, reward, done, action
