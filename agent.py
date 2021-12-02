26ac6b52668216b65a6ff859e324f6ba38afe765
Lilly swagpants Lees <climbgeekv7@gmail.com>
2021-12-02 15:16:57 +0000

    update convolutional layers and forward function

--- agent.py
+++ agent.py
@@ -28,10 +28,10 @@ class ReplayMemory():
 
 
 
-class NeuralNetwork(nn.Module):
+class DQN(nn.Module): #deep Q network class
 
     def __init__(self):
-        super(NeuralNetwork, self).__init__()
+        super(DQN, self).__init__()
 #10 x 24 input image for conv1
         self.number_of_actions = 5 #5 diffrent options for output
         self.gamma = 0.99
@@ -41,29 +41,20 @@ class NeuralNetwork(nn.Module):
         self.replay_memory_size = 10000
         self.minibatch_size = 32
 
-        self.conv1 = nn.Conv2d(4, 32, 8, 4)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(32, 64, 4, 2)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.conv3 = nn.Conv2d(64, 64, 3, 1)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.fc4 = nn.Linear(3136, 512)
-        self.relu4 = nn.ReLU(inplace=True)
-        self.fc5 = nn.Linear(512, self.number_of_actions)
+        self.conv1 = nn.Conv2d(1, 6, 10, 24)
+        self.conv2 = nn.Conv2d(6, 16, 10, 24)
+        self.fc1 = nn.Linear(16 * 10 * 24, 120)  
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, self.number_of_actions)
 
-    def forward(self, x): #N,C,H,W
-        out = self.conv1(x)
-        out = self.relu1(out)
-        out = self.conv2(out)
-        out = self.relu2(out)
-        out = self.conv3(out)
-        out = self.relu3(out)
-        out = out.view(out.size()[0], -1)
-        out = self.fc4(out)
-        out = self.relu4(out)
-        out = self.fc5(out)
+    def forward(self, x): 
+        x = F.max_pool2d(F.relu(self.conv1(x)), (10, 24))
+        x = torch.flatten(x, 1) 
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-        return out
 
 class Actions(NeuralNetwork):
     def __init__(self):
