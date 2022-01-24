b65c284e17faf63bb7dfbdc3363fcb3c98983de5
Lilly swagpants Lees <climbgeekv7@gmail.com>
2022-01-24 11:08:13 +0000

    still not trinaing

--- Enviroment.py
+++ Enviroment.py
@@ -8,41 +8,41 @@ import time
 class Tetramino():
     def __init__(self):
         self.x_move, self.y_move = [0,-1]
-        self.Tetraminos = {"I": {0 : [[2 , 3 ], [2 , 4 ], [2, 5 ], [2, 6 ]], 
+        self.Tetraminos = {0: {0 : [[2 , 3 ], [2 , 4 ], [2, 5 ], [2, 6 ]], #I
                                 90 : [[0 , 5 ], [1 , 5 ], [2 , 5], [3 , 5]],
                                 180 : [[2 , 3 ], [2 , 4 ], [2, 5 ], [2, 6 ]], 
                                 270 : [[0 , 5 ], [1 , 5 ], [2 , 5], [3 , 5]]  },
 
-                    "J": {0 :[[1 , 3 ], [1 , 4 ], [1, 5 ], [2 , 5 ]], 
+                    2: {0 :[[1 , 3 ], [1 , 4 ], [1, 5 ], [2 , 5 ]], #J
                     90 : [[0 , 4 ], [1 , 4 ], [2, 4 ], [2 , 3 ]], 
                     180 :[[1 , 3 ], [2 , 4 ], [2, 5 ], [2 , 3 ]], 
                     270 :[[0 , 4 ], [1 , 4 ], [2, 4 ], [0 , 5 ]]}, 
 
-                    "L": {0 :[[1 , 3 ], [1 , 4 ], [1, 5 ], [2 , 3 ]], 
+                    1: {0 :[[1 , 3 ], [1 , 4 ], [1, 5 ], [2 , 3 ]], #L
                     90 : [[0 , 3 ], [1 , 4 ], [2, 4 ], [0 , 4]], 
                     180 :[[1 , 5 ], [2 , 4 ], [2, 5 ], [2 , 3 ]], 
                     270 :[[0 , 4 ], [1 , 4 ], [2, 4 ], [2 , 5 ]]},
 
-                    "T": {0 :[[1 , 3 ], [1 , 4 ], [1, 5 ], [2 , 4 ]], 
+                    3: {0 :[[1 , 3 ], [1 , 4 ], [1, 5 ], [2 , 4 ]], #T
                     90 : [[0 , 4 ], [1 , 4 ],[1, 3], [2 , 4 ]], 
                     180 :[[2 , 3 ], [2 , 4 ],[2, 5 ], [1 , 4 ]], 
                     270 :[[0 , 4 ], [1 , 4 ],[1, 5 ], [1 , 3]]}, 
 
-                    "O": {0 : [[1 , 4], [2 , 4], [2, 5], [1, 5]]}, 
+                    4: {0 : [[1 , 4], [2 , 4], [2, 5], [1, 5]]}, #0
 
-                    "S": {0 : [[1 , 5], [1 , 4],[2, 3 ], [2 , 4]], 
+                    5: {0 : [[1 , 5], [1 , 4],[2, 3 ], [2 , 4]], #S
                         90 : [[0 , 4 ], [1 , 4 ],[1, 5 ], [2 , 5 ]],
                         180 : [[1 , 5], [1 , 4],[2, 3 ], [2 , 4]], 
                         270 : [[0 , 4 ], [1 , 4 ],[1, 5 ], [2 , 5 ]]}, 
 
-                    "Z": {0 : [[1 , 3 ], [1, 4],[2, 4 ], [2 , 5]], 
+                    6: {0 : [[1 , 3 ], [1, 4],[2, 4 ], [2 , 5]], #Z
                                 90 : [[0 , 5 ], [1 , 5],[1, 4 ], [2 , 4 ]],
                                 180 : [[1 , 3 ], [1, 4],[2, 4 ], [2 , 5]], 
                                 270 : [[0 , 5 ], [1 , 5],[1, 4 ], [2 , 4 ]] }} 
       
                          
-        self.Bag = ["I", "L", "J", "T", "O", "S", "Z"]
-        self.current_tet = random.choice(self.Bag)
+        #self.Bag = ["I", "L", "J", "T", "O", "S", "Z"] 
+        self.current_tet = random.randint(0,6) #I,L,J,T,O,S,Z
         self.rotations = [0, 90, 180, 270]
         self.rotation_index = 0
         self.rotation = self.rotations[self.rotation_index]
@@ -55,14 +55,14 @@ class Tetramino():
         self.rotation_index = 0
         self.temp_y_move = 0
         self.dropped = False
-        self.current_tet = random.choice(self.Bag)
+        self.current_tet = random.randint(0,5)
         
 
     def get_current_tet_coords(self, roation): #given rotation gets the coordanats for drawing the current tet
             return self.Tetraminos[self.current_tet][roation]
             
     def generate_new_tet(self):
-            self.current_tet = random.choice(self.Bag)
+            self.current_tet = random.randint(0,5)
 
 
 class CustomEnv(gym.Env, Tetramino):
@@ -138,6 +138,7 @@ class CustomEnv(gym.Env, Tetramino):
     reward = self.calc_reward(lc)
 
     if 1 in self.observation_space[0]:
+        print("not on my lawn")
         self.playing = False
         reward = - reward
     
@@ -275,7 +276,7 @@ class CustomEnv(gym.Env, Tetramino):
       self.rotate(-1)
 
   def rotate(self, dir): 
-        if self.current_tet == "O":
+        if self.current_tet == 4:
             self.rotation_index = 0
         else:
             if self.rotation_index == 3 and dir == 1:
--- TetAgent.py
+++ TetAgent.py
@@ -1,4 +1,5 @@
 import gym
+import tqdm
 import math
 import random
 import numpy as np
@@ -55,72 +56,59 @@ class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
         
+        self.conv1_board = nn.Conv2d(1, 16, kernel_size=3, stride=1)
+        self.conv2_board = nn.Conv2d(16, 32, kernel_size=3, stride=1)
+        self.conv3_board = nn.Conv2d(32, 6, kernel_size=3, stride=1)
+
+
         
-        self.conv1_board = nn.Conv2d(240, 16, kernel_size=5, stride=2)
-        self.conv2_board = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.conv3_board = nn.Conv2d(32, 6, kernel_size=5, stride=2)
-
-        self.conv1_piece = nn.Conv2d(1, 16, kernel_size=5, stride=2)
-        self.conv2_piece = nn.Conv2d(16, 6, kernel_size=5, stride=2)
-
-        self.fc1 = nn.Linear(1, 32)
-        self.fc2 = nn.Linear(32, 6)
+        self.fc1 = nn.Linear(433, 2048)
+        self.fc2 = nn.Linear(2048, 6)
         
 
         self.flatten = nn.Flatten()
 
-    def flt(self, arr):
+    def flt_totns(self, arr):
         flt = []
         for l in arr:
             flt.extend(l)
-        return flt
+        return torch.tensor(flt).unsqueeze(0).unsqueeze(0).unsqueeze(0).to(device).float()
 
-    def forward(self, states): #inputs to conv layers should be Tensors not list. convert list => tensor 
+    def forward(self, states): #inputs to conv layers should be Tensors not list. convert list => tensor
+
         board, piece = states
-        board = self.flt(board)
-        embed_board = flatten(self.conv3_board(self.conv2_board(self.conv1_board(board))))
-        embed_piece = flatten(self.conv2_piece(self.conv1_piece(piece)))
-        embed_joined = torch.cat([embed_board, embed_piece])
-
-        return self.fc2(self.fc1(embed_joined))
-
-'''class DQN(nn.Module):
-
-    def __init__(self, h, w, outputs):
-        super(DQN, self).__init__()
+        print(board)
+        exit
+        #board = self.flt_totns(board)
+        #embed board 2
+        embed_piece = torch.tensor(piece).unsqueeze(0).unsqueeze(0).to(device).float() #some reason getting diffrent dims
+        #board = torch.FloatTensor(board).unsqueeze(0).unsqueeze(0).to(device)
+        #print(embed_piece)
+        if type(piece) == int:
+            board = torch.FloatTensor(board).unsqueeze(0).unsqueeze(0).to(device) #issue
+           # board = self.flt_totns(board) #breaks code, Kernal size to big (3x 3) 
+            print(board.shape)
+            embed_piece = torch.tensor(piece).unsqueeze(0).unsqueeze(0).to(device).float()
+            
+        embed_board = self.flatten(self.conv3_board(self.conv2_board(self.conv1_board(board))))
         
-        
-        self.conv1 = nn.Conv2d(((10,24),1), 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        if type(piece) != int:
+            #print(f"BS {embed_board.shape}")
+            #print(f"PS {piece}") #[[1],[2],[7].....] etc
+            #print(f"EB {embed_board}")
+            embed_piece = piece #[128, 1]
+            
+  
+        #print(f"peice Shape: {embed_piece.shape}") 
+        #print(f"Board Shape: {embed_board.shape}")
+        #print(f"Peice {embed_piece}") #[[1],[2],[7].....] etc
+        #print(f"board {embed_board}")
 
-    
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
-
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
-        self.head = nn.Linear(linear_input_size, outputs)
-
-
-    def forward(self, x):
-        x = x.to(device)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1)) '''
-
-
-env.reset()
-'''plt.figure()
-plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
-           interpolation='none')
-plt.title('Example extracted screen')
-plt.show()'''
+        embed_joined = torch.cat([embed_board, embed_piece],dim=1)
+        #print(f"shape of both: {embed_joined.shape}") #[1,433] when playing this is ok
+        # [1, 73712] when training and len of embed
+        return torch.argmax(self.fc2(self.fc1(embed_joined)))
+       
 
 BATCH_SIZE = 128
 GAMMA = 0.999
@@ -129,19 +117,11 @@ EPS_END = 0.05
 EPS_DECAY = 200
 TARGET_UPDATE = 10
 
-# Get screen size so that we can initialize layers correctly based on shape
-# returned from AI gym. Typical dimensions at this point are close to 3x40x90
-# which is the result of a clamped and down-scaled render buffer in get_screen()
-#init_screen = get_screen()
-#_, _, screen_height, screen_width = init_screen.shape
 screen_height = env.board_height
 screen_width = env.board_width
 
 # Get number of actions from gym action space
 n_actions = env.action_space.n
-
-#policy_net = DQN(24, 10, n_actions).to(device)
-#target_net = DQN(24, 10, n_actions).to(device)
 
 policy_net = DQN().to(device)
 target_net = DQN().to(device)
@@ -155,7 +135,7 @@ memory = ReplayMemory(10000)
 steps_done = 0
 
 def ran_move():
-    m = [0,1,1,1,1,1,2,2,2,2,2,3,3,4,4,5]
+    m = [0,1,1,1,2,2,2,3,3,4,5]
     return random.choice(m)
 
 def select_action(state):
@@ -166,48 +146,51 @@ def select_action(state):
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
-            # t.max(1) will return largest column value of each row.
-            # second column on max result is index of where max element was
-            # found, so we pick action with the larger expected reward.
-            return policy_net(state).max(1)[1].view(1, 1)
+            return policy_net(state).item()
     else:
-        #return torch.tensor([[random.randrange(6)]], device=device, dtype=torch.long)
         return ran_move()
 
 
 episode_durations = []
 
 def optimize_model():
+    print("optimizing")
     if len(memory) < BATCH_SIZE:
         return
     transitions = memory.sample(BATCH_SIZE)
-    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-    # detailed explanation). This converts batch-array of Transitions
-    # to Transition of batch-arrays.
     batch = Transition(*zip(*transitions))
-
-    # Compute a mask of non-final states and concatenate the batch elements
-    # (a final state would've been the one after which simulation ended)
+    
     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
                                           batch.next_state)), device=device, dtype=torch.bool)
-    non_final_next_states = torch.cat([s for s in batch.next_state
-                                                if s is not None])
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
+    
+    non_final_next_pieces = torch.cat([torch.tensor(s[1]).unsqueeze(0) for s in list(batch.next_state)
+                                                if s is not None]).float()
+    non_final_next_board = torch.cat([torch.tensor(s[0]) for s in list(batch.next_state)
+                                                if s is not None]).float()
 
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken. These are the actions which would've been taken
-    # for each batch state according to policy_net
-    state_action_values = policy_net(state_batch).gather(1, action_batch)
+    board_batch = torch.cat([torch.tensor([s[0]]) for s in batch.state]).squeeze(0).squeeze(0).float() 
+    #print("BBBBB", board_batch)
+    #for s in batch.state:
+    #    print(f"BS E {s[1]}")
+    piece_batch = torch.cat([torch.tensor([s[1]]).unsqueeze(0) for s in batch.state]).to(device).float() 
+    
+    #piece_batch = piece_batch.permute(1, 0)
+    
+    #print(f"PB {piece_batch}")
+    
+    #embed board, emebed peice. cant cat because diffre number of dims. 
+    action_batch = torch.cat([torch.tensor(s).unsqueeze(0) for s in batch.action]).to(device)
+    reward_batch = torch.cat(batch.reward).to(device)
 
-    # Compute V(s_{t+1}) for all next states.
-    # Expected values of actions for non_final_next_states are computed based
-    # on the "older" target_net; selecting their best reward with max(1)[0].
-    # This is merged based on the mask, such that we'll have either the expected
-    # state value or 0 in case the state was final.
+    #print(f"PB {piece_batch.shape}")
+    #l = torch.cat([torch.tensor(policy_net((i,l))) for i, l in board_batch, piece_batch]).to(device).float().gather(1, action_batch)
+    #for i, l in board_batch, piece_batch:
+    #    policy_net((i,l)).gather(1, action_batch)
+
+    state_action_values = policy_net((board_batch, piece_batch)).gather(1, action_batch) #THIS LINE KILL ME #issue
+
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    next_state_values[non_final_mask] = target_net((non_final_next_board, non_final_next_pieces)).max(1)[0].detach()
     # Compute the expected Q values
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
@@ -222,26 +205,30 @@ def optimize_model():
         param.grad.data.clamp_(-1, 1)
     optimizer.step()
 
-num_episodes = 50
+
+
+num_episodes = 100
 for i_episode in range(num_episodes):
     # Initialize the environment and state
     env.reset()
     cur_tet, board = env.get_state()
+    #state = [torch.FloatTensor(board), torch.FloatTensor(cur_tet)]
     state = [board, cur_tet]
 
-    for t in count():
+    for t in tqdm.tqdm(range(100)):
         # Select and perform an action
         action = select_action(state)
-        #_, reward, done, _ = env.step(action.item())
-        _, reward, done, _ = env.step(action)
+
+        _, reward, playing, _ = env.step(action)
         reward = torch.tensor([reward], device=device)
         
         env.Render_m()
-        # Observe new state
-        #last_screen = current_screen
-        #current_screen = get_screen()
-        if not done:
-            next_state = env.get_State()
+        time.sleep(0.1)
+    
+        if playing:
+            cur_tet, board = env.get_state()
+            next_state = [board, cur_tet]
+
         else:
             next_state = None
 
@@ -252,17 +239,19 @@ for i_episode in range(num_episodes):
         state = next_state
 
         # Perform one step of the optimization (on the policy network)
-        optimize_model()
-        if done:
+        
+        if not playing:
+            print(playing)
             episode_durations.append(t + 1)
             #plot_durations()
             break
         
         env.Render_m()
     # Update the target network, copying all weights and biases in DQN
+    optimize_model()
     if i_episode % TARGET_UPDATE == 0:
         target_net.load_state_dict(policy_net.state_dict())
-
+    
 print('Complete')
 #env.render()
 env.close()
