def896ec436a17ead19d313cf86a60d701479499
Lilly swagpants Lees <climbgeekv7@gmail.com>
2022-02-02 09:46:31 +0000

    cleaned unessary imports and code, commented decriptions of each line

--- Agent/Train.py
+++ Agent/Train.py
@@ -1,83 +1,92 @@
 from TetAgent import *
+import tqdm
+import wandb
 
-num_episodes = int(input("Number of epidodes: "))
+wandb.init(project="Tetris-RL", entity="forestlees") #importing wandb project
 
-Speed = (100 - int(input("speed 0-100: "))) / 100
-ep_leng = int(input("Episode length [in steps]: "))
+wandb.config = { #configuring wandb graph
+  "learning_rate": 0.001,
+  "epochs": 100,
+  "batch_size": 128
+}
 
-policy_net = torch.load('nets/policy_net.ckpt')
-target_net = torch.load('nets/target_net.ckpt')
+num_episodes = int(input("Number of epidodes: ")) #user deffines how many episodes will train
+ep_leng = int(input("Episode length [in steps]: ")) #user deffines the length of each episode
 
-losses = []
-rewards = []
-
+policy_net = torch.load('Nets/policy_net.ckpt') #loading latest trained policy net
+target_net = torch.load('Nets/target_net.ckpt') #loading lastest trained target net
 
 for i_episode in range(num_episodes):
+
     print(f'EPISODE {i_episode}')
-    # Initialize the environment and state
-    env.reset()
-    cur_tet, board = env.get_state()
+    env.reset() # Initialize the environment and state
+
+    cur_tet, board = env.get_state() #get current states 
     state = [board, cur_tet]
-    avg_r = []
+    avg_r = [] #creating a list of all rewards in a episode
+    score_ = 0 #total score for the game
+
     for t in tqdm.tqdm(range(ep_leng)):
         # Select and perform an action
         action = select_action(state)
+        _, reward, playing, _ = env.step(action) 
 
-        _, reward, playing, _ = env.step(action)
-        reward = torch.tensor([reward], device=device)
-        avg_r.append(int(reward[0]))
-
+        avg_r.append(reward) #add reward to list of rewards. 
+        reward = torch.tensor([reward], device=device) #convert int reward to tensor
         env.Render_m()
-        time.sleep(Speed)
-    
+
+        #check if state is terminal and update next state array
         if playing:
             cur_tet, board = env.get_state()
             next_state = [board, cur_tet]
-
         else:
             next_state = None
 
         # Store the transition in memory
         memory.push(state, action, next_state, reward)
         env.Render_m()
-        # Move to the next state
-        state = next_state
 
+        state = next_state # Move to the next state
+        score_ = env.score # get current score
+
+        #check if state is terminal and update episode duration
         if not playing:
             episode_durations.append(t + 1)
-            #plot_durations()
             break
         
         env.Render_m()
+
     # Update the target network, copying all weights and biases in DQN
-    
-    rewards.append(sum(avg_r) / ep_leng)
     loss = optimize_model()
+
+    #update wandb graphs
+    wandb.log({"score": score_})
+    wandb.log({"reward": sum(avg_r) / ep_leng})
     if type(loss) == int:
-        losses.append(loss)
+        wandb.log({"loss": loss})
     else:
-        losses.append(0)
-        
+        wandb.log({"loss": 0})
+    
+    #update target net every 10th episode
     if i_episode % TARGET_UPDATE == 0:
         target_net.load_state_dict(policy_net.state_dict())
-    
 
-print(losses)
-print(rewards)
+    #save changed netowrks every 200 episodes 
+    if i_episode % 200 == 0:
+        print("Saving")
+        torch.save(policy_net, 'Nets/policy_net.ckpt')
+        torch.save(target_net, 'Nets/target_net.ckpt') 
 
-#torch.save(policy_net, 'nets/policy_net.ckpt')
-#torch.save(target_net, 'nets/target_net.ckpt')
+#save changed networks once all epusodes are finsihed 
+torch.save(policy_net, 'Nets/policy_net.ckpt')
+torch.save(target_net, 'Nets/target_net.ckpt') 
+
+
 print('Complete')
+env.close() #close enviroment
 
 
-env.close()
-pygame.quit()
 
-plt.ion()
-plt.scatter(rewards, losses)
-plt.xlabel("Reward")
-plt.ylabel("Loss")
-plt.show()
 
 
 
